03e67eb756e6a9475493155e9a15434f859873c2
jellewillekes <94389770+jellewillekes@users.noreply.github.com>
2024-08-14 10:53:58 +0200

    Update loader.py

--- data/raw/loader.py
+++ data/raw/loader.py
@@ -11,14 +11,22 @@ from utils.load import load_mappings_from_yaml, load_api_key, project_root
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-def request_data(country, league_name, league_id, season, request_counter, start_time, api_key):
+def request_data(country, league_name, league_id, season, data_type, request_counter, start_time, api_key):
     directory_path = os.path.join(project_root(), 'data', 'raw', country, league_name, season)
-    file_path = os.path.join(directory_path, 'league_data.json')
+    if data_type == "standings":
+        file_path = os.path.join(directory_path, 'league_data.json')
+        endpoint = f"/standings?league={league_id}&season={season}"
+    elif data_type == "fixtures":
+        file_path = os.path.join(directory_path, 'fixtures_data.json')
+        endpoint = f"/fixtures?league={league_id}&season={season}"
+    else:
+        logging.error(f"Unknown data_type {data_type} for {league_name} {season}.")
+        return None, request_counter, start_time, False
 
     if os.path.isfile(file_path):
         with open(file_path, 'r') as file:
             data_dict = json.load(file)
-        logging.info(f"Loaded data from existing file for {league_name} {season}.")
+        logging.info(f"Loaded {data_type} data from existing file for {league_name} {season}.")
         return data_dict, request_counter, start_time, True
     else:
         # Rate limiting
@@ -37,7 +45,7 @@ def request_data(country, league_name, league_id, season, request_counter, start
                 'x-rapidapi-host': "v3.football.api-sports.io",
                 'x-rapidapi-key': api_key
             }
-            conn.request("GET", f"/standings?league={league_id}&season={season}", headers=headers)
+            conn.request("GET", endpoint, headers=headers)
             res = conn.getresponse()
             data = res.read()
 
@@ -45,22 +53,22 @@ def request_data(country, league_name, league_id, season, request_counter, start
 
             # Check for errors in the response
             if data_dict.get("errors"):
-                logging.error(f"Error in response for {league_name} {season}: {data_dict['errors']}")
+                logging.error(f"Error in response for {league_name} {season} ({data_type}): {data_dict['errors']}")
                 return None, request_counter, start_time, False
 
             # Check if the response is empty
             if not data_dict['response']:
-                logging.info(f"No data in response for {league_name} {season}.")
+                logging.info(f"No data in response for {league_name} {season} ({data_type}).")
                 return None, request_counter, start_time, True
 
             os.makedirs(directory_path, exist_ok=True)
             with open(file_path, 'w') as file:
                 json.dump(data_dict, file, indent=4)
-            logging.info(f"Requested new data and saved to file for {league_name} {season}.")
+            logging.info(f"Requested new {data_type} data and saved to file for {league_name} {season}.")
 
             request_counter += 1
         except Exception as e:
-            logging.error(f"Error requesting data for {league_name} {season}: {e}")
+            logging.error(f"Error requesting {data_type} data for {league_name} {season}: {e}")
             return None, request_counter, start_time, False
 
         return data_dict, request_counter, start_time, True
@@ -78,19 +86,23 @@ def request_raw_data(country):
         league_id = league_info['id']
         season_start = league_info['season_start']
         season_end = league_info['season_end']
+        data_types = league_info.get('data_types', [])
 
-        if 'standings' in league_info['data_types']:
-            for season in range(season_start, season_end + 1):
-                logging.info(f"Processing {league_name} for the {season} season.")
+        for season in range(season_start, season_end + 1):
+            logging.info(f"Processing {league_name} for the {season} season.")
+            for data_type in data_types:
+                logging.info(f"Requesting {data_type} data for {league_name} {season}.")
                 result, request_counter, start_time, continue_processing = request_data(country,
                                                                                         league_name,
                                                                                         str(league_id),
                                                                                         str(season),
+                                                                                        data_type,
                                                                                         request_counter,
                                                                                         start_time,
                                                                                         api_key)
                 if not continue_processing:
-                    logging.info(f"Stopping further requests due to error in response for {league_name} {season}.")
+                    logging.info(
+                        f"Stopping further requests due to error in response for {league_name} {season} ({data_type}).")
                     return
 
 
